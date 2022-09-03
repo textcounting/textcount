dadcd075114eb4f1804ab8f999025eaa51981a25
parkheesung <parkheesung@eyemcast.com>
2022-09-03 10:37:53 +0900

    기능 추가

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -10,12 +10,54 @@ function MainPage(props:any) {
 
     const fnReset = () => {
         setText("");
+        fnSetValue("");
+    };
+
+    const fnSetValue = (str:string):void => {
         let input = document.getElementById("content") as HTMLInputElement;
         if (input !== null && input !== undefined) {
-            input.value = "";
+            input.value = str;
             input.focus();
         }
     };
+
+    const fnLiveRemove = () => {
+        let tmp = txt;
+        if (tmp !== null && tmp !== undefined && tmp !== "") {
+            let result = [];
+            let arr = tmp.split("\n");
+            if (arr !== null && arr !== undefined && arr.length > 0) {
+                for(let i = 0; i < arr.length; i++) {
+                    if (arr[i] !== null && arr[i] !== undefined && arr[i].trim() !== "") {
+                        result.push(arr[i]);
+                    }
+                }
+
+                tmp = result.join("\n");
+                fnSetValue(tmp);
+                setText(tmp);
+            }
+        }
+    };
+
+    const fnLiveAdd = () => {
+        let tmp = txt;
+        if (tmp !== null && tmp !== undefined && tmp !== "") {
+            let result = [];
+            let arr = tmp.split("\n");
+            if (arr !== null && arr !== undefined && arr.length > 0) {
+                for(let i = 0; i < arr.length; i++) {
+                    result.push(arr[i] + "\n");
+                }
+
+                tmp = result.join("\n");
+                fnSetValue(tmp);
+                setText(tmp);
+            }
+        }
+    };
+
+    const gotoGithub = () => { document.location.href = "https://github.com/parkheesung/textcount"; }
 
     return (
         <Container className="p-3">
@@ -29,14 +71,16 @@ function MainPage(props:any) {
         <Column size="12">
             <Card>
             <h5 className="card-title">Counting : {txt.length}</h5>
-            <textarea name="content" id="content" className="form-control" rows={12} onChange={counting} defaultValue={txt} />
+            <textarea name="content" id="content" className="form-control" rows={15} onChange={counting} defaultValue={txt} />
             </Card>
         </Column>
         </Row>
         <Row>
         <Column size="12">
             <Card>
-
+            <Button type="button" id="btnLiveRemove" className="btn-primary btn-sm mr-3" onClick={fnLiveRemove}>줄간격제거</Button>
+            <Button type="button" id="btnLiveAdd" className="btn-warning btn-sm" onClick={fnLiveAdd}>1줄간격주기</Button>
+            <code className="right hand" onClick={gotoGithub}>https://github.com/parkheesung/textcount</code>
             </Card>
         </Column>
         </Row>
